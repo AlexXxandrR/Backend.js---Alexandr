9d25477a7563808b11d58e03ebd6795c33dc7e0c
Alexandr Dalinenkov <likezerg18@inbox.ru>
2024-08-12 16:39:30 +0300

    Задача маппер добавлена

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
-const name = 'admin';
-let level;
-
-switch (name) {
-    case 'user':
-    case 'client':
-      level = 'First level';
-      break;
-
-    case 'admin':
-    case 'manager':
-      level = 'Two level';
-        break;
-
-    default:
-        level = 'Unknown name'
-        break;
-}
-console.log(`Роль: ${name}
-Уровень доступа: ${level}`);
-console.log(level)
+const person = {
+    age: 10,
+    name: 'Alex',
+};
+person.brother = 'Vlad';
+person['brother - 2'] = 'Олег';
+delete person.brother;
+delete person['brother - 2']
+console.log(person)
+person.brotherTwo = {age: 7, name: 'Nickolas' };
+console.log(person)
+person.brotherTwo.adress = 'Sankt-Petrburg';
+console.log(person)
+person.brotherTwo.age = 11;
+console.log(person)

58b9d02cf86d3e41dfe2aa46f883008c29a6a70a
Alexandr Dalinenkov <likezerg18@inbox.ru>
2024-08-19 18:29:09 +0300

    Задача по циклу for i

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
-const person = {
-    age: 10,
-    name: 'Alex',
-};
-person.brother = 'Vlad';
-person['brother - 2'] = 'Олег';
-delete person.brother;
-delete person['brother - 2']
-console.log(person)
-person.brotherTwo = {age: 7, name: 'Nickolas' };
-console.log(person)
-person.brotherTwo.adress = 'Sankt-Petrburg';
-console.log(person)
-person.brotherTwo.age = 11;
-console.log(person)
+sum = 0;
+for (let i = 0; i < 10; i++) {
+    if (i % 3 === 0 || i % 5 === 0){
+        sum += i;
+    }
+}
+console.log(sum);

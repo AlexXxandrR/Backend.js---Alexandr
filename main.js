cfc2190f2a289f2b805ef86283315887b961f4ee
Alexandr Dalinenkov <likezerg18@inbox.ru>
2024-08-20 17:35:39 +0300

    Задача по циклу for i; N - 8

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-const arr = [0, 2, 3, 11, 21];
-const middle = Math.floor(arr.length / 2);
-const massive = [];
+const arr = [0, -2, 3, -11, 21];
 
-for (let i = middle; i < arr.length; i++) {
-    massive.push(arr[i]);
+let Positive = 0;
+let Negative = 0;
+
+for (let i = 0; i < arr.length -1; i++) {
+    if (arr[i] > 0) {
+        Positive++;
+    }else if (arr[i] < 0){
+        Negative++;
+    }
 }
-console.log(massive);
+console.log(`Положительные числа: ${Positive}
+Отрийательные числа: ${Negative}
+`);
+
 

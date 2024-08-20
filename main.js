6ccb04bc9cb3bba88b62650bee2bf9ee3996827b
Alexandr Dalinenkov <likezerg18@inbox.ru>
2024-08-20 16:46:52 +0300

    Задача по циклу for i; N - 6

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
-const arr = [1.1 , 2.2, -2, 3.3, -1, 5.5, 5.99, 7.49, 7.9999, -2.2];
-const res = [];
-let count = 0;
+const arr = [0, 2, 3, 11, 21];
+const middle = Math.floor(arr.length / 2);
+const massive = [];
 
-for (let i = 0; i < arr.length; i++) {
-    if(arr[i] > 0) {
-        count++;
-    if(count > 3) {
-        res.push(Math.round(arr[i]));
-
-    }else {
-     res.push(arr[i]);
-    }
-    }else {
-        res.push(arr[i]);
-    }
+for (let i = 0; i < middle; i++) {
+    massive.push(arr[i]);
 }
-console.log(res);
+console.log(massive);
+

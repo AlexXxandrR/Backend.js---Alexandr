8f2589e47ede409fed83027803c1b8823d525515
Alexandr Dalinenkov <likezerg18@inbox.ru>
2024-08-20 16:54:35 +0300

    Задача по циклу for i; N - 7

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const arr = [0, 2, 3, 11, 21];
 const middle = Math.floor(arr.length / 2);
 const massive = [];
 
-for (let i = 0; i < middle; i++) {
+for (let i = middle; i < arr.length; i++) {
     massive.push(arr[i]);
 }
 console.log(massive);

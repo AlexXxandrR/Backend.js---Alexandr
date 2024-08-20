84e80a3a247adf9d25f501de0d69851f31440638
Alexandr Dalinenkov <likezerg18@inbox.ru>
2024-08-20 14:50:50 +0300

    Задача по циклу for i; N - 4

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-let arr = [1, 9, 2, 3, 7 ,5];
+const arr = [-10, -4, 0, 1, 4, 10, -3];
+const res = [];
 
 for (let i = 0; i < arr.length; i++) {
-    arr[i] = i;
+    if(arr[i] < 0) {
+        res.push(Math.abs(arr[i]));
+    } else if(arr[i] > 0)  {
+        res.push(arr[i] * 1.1);
+    } else {
+        res.push(arr[i]);
+    }
 }
-console.log(arr);
+console.log(res);

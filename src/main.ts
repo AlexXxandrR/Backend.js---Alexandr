f061a9d3fcd2ed173ea645d745e1040b0d8a824d
Alexandr Dalinenkov <likezerg18@inbox.ru>
2024-09-05 18:24:40 +0300

    Проверка поля surname, с помощью операторов ?? ||

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,17 @@
-type UserProfile = {
-  name: string;
-  age: number;
-  surname?: string | null;
-};
-const user1: UserProfile = {
-  name: 'Ruslan',
-  age: 22,
-  surname: 'Semak',
-};
-const user2: UserProfile = {
-  name: 'Alexandr',
-  age: 40,
-  surname: null,
-};
-const user3: UserProfile = {
-  name: 'Vladimir',
-  age: 33,
-};
-const user4: UserProfile = {
-  name: 'Vladimir',
-  age: 33,
-  surname: '',
+const split = (word: string) => {
+  const letters: string[] = [];
+  const digits: string[] = [];
+
+  for (const char of word) {
+    if (isNaN(Number(char))) {
+      letters.push(char);
+    } else {
+      digits.push(char);
+    }
+  }
+
+  return { letters, digits };
 };
 
-const CheckSurname = (user: UserProfile) => {
-  console.log((user.surname ?? 'Не обнаружено') || 'Пусто');
-};
-CheckSurname(user1);
-CheckSurname(user2);
-CheckSurname(user3);
-CheckSurname(user4);
+const result = split('abc123def456');
+console.log(result); // { letters: ['a', 'b', 'c', 'd', 'e', 'f'], digits: ['1', '2', '3', '4', '5', '6'] }

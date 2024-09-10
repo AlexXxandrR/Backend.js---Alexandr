08543fe790b332b4d828cf01d2e2429bf7ad462d
Alexandr Dalinenkov <likezerg18@inbox.ru>
2024-09-10 16:00:09 +0300

    Задание - Теперь они могут быть ещё и null, undefined, string, boolean

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-const ObjectValues = (obj: { [key: string]: number }): number => {
+const ObjectValues = (obj: { [key: string]: any }): number => {
   let sum = 0;
 
   for (const key in obj) {
-    sum += obj[key];
+    const value = obj[key];
+    if (typeof value === 'number') {
+      sum += value;
+    }
   }
 
   return sum;
@@ -11,8 +14,11 @@ const ObjectValues = (obj: { [key: string]: number }): number => {
 const data = {
   a: 10,
   b: 20,
-  c: 5,
+  c: null,
   d: 15,
+  e: 'string',
+  f: true,
+  g: undefined,
 };
 
 console.log(ObjectValues(data));

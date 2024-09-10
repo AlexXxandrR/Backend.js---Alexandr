8aca2eeb3c20766a2ad78ce81c97f31d4d7eb795
Alexandr Dalinenkov <likezerg18@inbox.ru>
2024-09-10 15:42:29 +0300

    Задание - сумма значений

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
-const arrays = (arr: number[]) => {
-  for (let i = 0; i < arr.length; i++) {
-    const num = arr[i];
-    let max = 1;
+const ObjectValues = (obj: { [key: string]: number }): number => {
+  let sum = 0;
 
-    for (let j = 1; j < num; j++) {
-      if (num % j === 0) {
-        max = j;
-      }
-    }
-
-    console.log(`Наибольший делитель для ${num} - ${max}`);
+  for (const key in obj) {
+    sum += obj[key];
   }
+
+  return sum;
 };
-const numbers: number[] = [1, 10, 11, 12, 21, 61, 85, 123];
-arrays(numbers);
+
+const data = {
+  a: 10,
+  b: 20,
+  c: 5,
+  d: 15,
+};
+
+console.log(ObjectValues(data));

4f2026dbf7c9491fccfa3054b5fc937092ddf18e
Alexandr Dalinenkov <likezerg18@inbox.ru>
2024-09-10 16:06:59 +0300

    Задание - билдер

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,8 @@
-const ObjectValues = (obj: { [key: string]: any }): number => {
-  let sum = 0;
+const sayBuilder = (greeting: string) => (name: string) => `${greeting}, ${name}!`;
 
-  for (const key in obj) {
-    const value = obj[key];
-    if (typeof value === 'number') {
-      sum += value;
-    }
-  }
+const sayHi = sayBuilder('Hi');
+const sayBye = sayBuilder('Bye');
 
-  return sum;
-};
-
-const data = {
-  a: 10,
-  b: 20,
-  c: null,
-  d: 15,
-  e: 'string',
-  f: true,
-  g: undefined,
-};
-
-console.log(ObjectValues(data));
+console.log(sayHi('Ruslan'));
+console.log(sayBye('Ruslan'));
+console.log(sayBye('Maxim'));

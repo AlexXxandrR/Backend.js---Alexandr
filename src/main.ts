e0aa387587678e196c53f48879f1503aeab84493
Alexandr Dalinenkov <likezerg18@inbox.ru>
2024-10-17 16:34:01 +0300

    Исправьте код. Массивы

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,24 @@
-const merge = (a: any, b: any, order: 1 | 2 = 1) => {
-  if (order === 1) {
-    return { ...a, ...b };
-  } else {
-    return { ...b, ...a };
-  }
-};
-const obj1 = { a: 10, b: 'b', c: 100, child: { name: 'ch', surname: 'hc' } };
-const obj2 = { a: 20, b: ['b'], d: 200, child: { aa: 'aa', bb: 'bb' }, merged: 'NO!' };
+const storage = [
+  'nick',
+  'nack',
+  'nock',
+  [
+    {
+      first: 'forecast',
+      child: null,
+    },
+    {
+      first: 'castfore',
+      child: null,
+    },
+    'zzz',
+  ],
+  'no-1',
+  'no-2',
+];
 
-console.log(merge(obj1, obj2));
-/* Вывод:
-{
-  a: 20,
-  b: [ 'b' ],
-  c: 100,
-  child: { aa: 'aa', bb: 'bb' },
-  d: 200,
-  merged: 'NO!'
-}
-*/
+const [, , , [, b1, cos], ...nos] = storage;
 
-console.log(merge(obj1, obj2, 1));
-/* Вывод:
-{
-  a: 20,
-  b: [ 'b' ],
-  c: 100,
-  child: { aa: 'aa', bb: 'bb' },
-  d: 200,
-  merged: 'NO!'
-}
-*/
-
-console.log(merge(obj1, obj2, 2));
-/* Вывод:
-{
-  a: 10,
-  b: 'b',
-  c: 100,
-  child: { name: 'ch', surname: 'hc' },
-  d: 200,
-  merged: 'YES!'
-}
-*/
+console.log(nos); // [ 'no-1', 'no-2' ]
+console.log(cos); // [ 'zzz' ]
+console.log(b1); // { first: 'castfore', child: null }

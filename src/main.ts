86e4a9b7bf981fa7aab60de4f00d81efd82ee21b
Alexandr Dalinenkov <likezerg18@inbox.ru>
2024-10-17 15:38:14 +0300

    Простой merge в новый объект

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-const sumObjectValues = (obj: Record<string, number>): number => {
-  let sum = 0;
-  Object.keys(obj).forEach((key) => {
-    sum += obj[key];
-  });
-  return sum;
+const mergeToNewObject = (a: any, b: any) => {
+  return { ...a, ...b };
 };
 
-const obj = { a: 10, b: 20, c: 30 };
-console.log(sumObjectValues(obj));
+const obj1 = { a: 10, b: 100, c: 1 };
+const obj2 = { a: 20, b: 200, e: 2 };
+const obj3 = mergeToNewObject(obj1, obj2);
+
+console.log(obj1); // { a: 10, b: 100, c: 1 }
+console.log(obj2); // { a: 20, b: 200, e: 2 }
+console.log(obj3); // { a: 20, b: 200, c: 1, e: 2 }

b440c695669646426d86059ffe6e3e5ee92b44ca
Alexandr Dalinenkov <likezerg18@inbox.ru>
2024-10-22 16:12:28 +0300

    Home Work: домашняя работа,задание №2

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
-enum Color {
-  red = 'Красный',
-  blue = 'Синий',
-  green = 'Зеленый',
-  orange = 'Оранжевый',
+enum Colors {
+  red = 'red',
+  black = 'black',
 }
-console.log(Object.values(Color));
+
+const mapper = (value: string): Colors => {
+  if (value === Colors.red || value === Colors.black) {
+    return value as Colors;
+  } else {
+    return Colors.red;
+  }
+};
+
+const color1: Colors = mapper('red'); // red, вернулось Colors.red, так как есть совпадение
+const color2: Colors = mapper('black'); // black, вернулось Colors.black, так как есть совпадение
+const color3: Colors = mapper('it is not a color'); // red, вернулось Colors.red "по умолчанию"

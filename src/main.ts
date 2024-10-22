e5c40ebd85dc0e864c4f56e107f6d54064275558
Alexandr Dalinenkov <likezerg18@inbox.ru>
2024-10-22 16:04:07 +0300

    Home Work: домашняя работа,задание №1

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,7 @@
-const countTypes = (...args: any[]): Record<string, number> => {
-  return args.reduce((acc, arg) => {
-    let type;
-
-    if (arg === null) {
-      type = 'null';
-    } else {
-      type = typeof arg;
-    }
-
-    if (acc[type]) {
-      acc[type] += 1;
-    } else {
-      acc[type] = 1;
-    }
-
-    return acc;
-  }, {});
-};
-
-const func = () => {};
-
-console.log(
-  countTypes(3, true, 'a', 1, {}, func, 4, [], undefined, false, 0, undefined, func, {}, ''),
-);
+enum Color {
+  red = 'Красный',
+  blue = 'Синий',
+  green = 'Зеленый',
+  orange = 'Оранжевый',
+}
+console.log(Object.values(Color));
